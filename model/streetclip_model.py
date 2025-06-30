4c1bd22faf0a3830f3efa1c4dacba5cd456e58c9
Karan <karan.dhing@gmail.com>
2025-06-30 14:40:51 +0530

    Reducing RAM usage for render

--- model/streetclip_model.py
+++ model/streetclip_model.py
@@ -3,33 +3,33 @@ from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
 import torch
 
-model = CLIPModel.from_pretrained("geolocal/StreetCLIP").half()
-processor = CLIPProcessor.from_pretrained("geolocal/StreetCLIP")
 
-model = model.to("cpu")
-model.eval()
 with open("model/location_tree.json", "r") as f:
     LOCATION_TREE = json.load(f)
 
-
 def predict_label(image, labels):
+    processor = CLIPProcessor.from_pretrained("geolocal/StreetCLIP")
+    model = CLIPModel.from_pretrained("geolocal/StreetCLIP").half().eval()
+
     inputs = processor(text=labels, images=image, return_tensors="pt", padding=True)
-    with torch.no_grad():
+    with torch.no_grad():  
         outputs = model(**inputs)
-        logits = outputs.logits_per_image.squeeze(0)
-        probs = logits.softmax(dim=0)
+    logits = outputs.logits_per_image.squeeze(0)
+    probs = logits.softmax(dim=0)
     best_idx = probs.argmax().item()
     return labels[best_idx], probs[best_idx].item()
-
 
 def predict_location_hierarchical(image_path):
     image = Image.open(image_path).convert("RGB")
 
+
     country_labels = list(LOCATION_TREE.keys())
     country, conf_country = predict_label(image, country_labels)
 
+
     city_labels = LOCATION_TREE[country]["cities"]
     city, conf_city = predict_label(image, city_labels)
+
 
     landmark = None
     conf_landmark = None
@@ -43,5 +43,5 @@ def predict_location_hierarchical(image_path):
         "city": city,
         "city_conf": conf_city,
         "landmark": landmark,
-        "landmark_conf": conf_landmark
+        "landmark_conf": conf_landmark,
     }
